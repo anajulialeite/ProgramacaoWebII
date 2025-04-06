b15c2acf483bb72013b5fb3d9d7059a92e4ac8c3
Ana Júlia Leite <leite.anajulia25@gmail.com>
2025-04-06 00:02:34 -0300

    Subindo conteúdo do semestre!

---
+++ Exercícios/Ex01/Ex01/Ex01/Personagem.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01
+{
+    internal class Personagem
+    {
+        private string nome;
+
+        private int vida;
+
+        private int ataque;
+
+        private int defesa;
+
+        public Personagem(string nome)
+        {
+            this.nome = nome;
+            this.vida = 100;
+            this.ataque = 50;
+            this.defesa = 30;
+            Console.WriteLine("Personagem: " + nome + ", criado com sucesso!");
+        }
+
+        //Regras de combate
+        public string getNome()
+        {
+            return nome;
+        }
+
+        public int getVida()
+        {
+            return vida;
+        }
+
+        public int getAtaque()
+        {
+            return ataque;
+        }
+
+        public int getDefesa()
+        {
+            return defesa;
+        }
+
+        public void Combate(int dano)
+        {
+            Random danoVariavel = new Random();
+            int ataqueFinal = this.defesa - danoVariavel.Next(0, dano);
+
+            if (ataqueFinal > 0)
+            {
+                vida -= ataqueFinal;
+                if (VerificarVida())
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(nome + ", leva dano de: " + ataqueFinal);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("vida: " + vida);
+                    Console.WriteLine("");
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine(nome + " Bloqueou o ataque");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("");
+            }
+
+        }
+
+        public bool VerificarVida()
+        {
+            if(vida <= 0)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                Console.WriteLine(nome + ". Se Lascou! :(");
+                Console.ForegroundColor = ConsoleColor.White;
+                return false;
+            }
+            return true;
+        }
+
+    }
+
+}
+
---
+++ Exercícios/Ex01/Ex01/Ex01/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool gameOver = false;
+
+            Personagem person1 = new Personagem("Lula");
+            Personagem Person2 = new Personagem("Bolsonaro");
+
+            Console.Clear();
+
+            do
+            {
+                if (person1.getVida() > 0 && Person2.getVida() > 0)
+                {
+                    person1.Combate(Person2.getAtaque());
+                    Person2.Combate(person1.getAtaque());
+                }
+                else if (person1.getVida() <= 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(Person2.getNome() + " Ganhou!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    gameOver = true;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(person1.getNome() + " Ganhou!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    gameOver = true;
+                }
+
+            } while (!gameOver);
+
+            Console.ReadLine();
+        }
+    }
+
+}
+
